777610542766463e7b801929f1f37ad2c74287c4
jochen@homeland <no-go@localhost>
2016-10-18 22:15:25 +0200

    license is bsd-3

--- UART-Smartwatch_App/app/src/main/java/click/dummer/UartSmartwatch/MainActivity.java
+++ UART-Smartwatch_App/app/src/main/java/click/dummer/UartSmartwatch/MainActivity.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2015, Nordic Semiconductor
  * All rights reserved.
  * 
- * 2016 - modified some parts by Jochen Peters
+ * 2016 - modified many parts by Jochen Peters
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
