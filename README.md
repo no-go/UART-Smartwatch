5c6f7cad93f1433b9e7de26dfc746e27c68cb5c2
jochen@homeland <no-go@localhost>
2016-12-26 21:39:04 +0100

    does not work on my PRO trinket - maybe buggy hardware or compile? memory leaks? A game is added on button2 2800ms press

--- README.md
+++ README.md
@@ -5,16 +5,8 @@ modified to display the power of the LiPo better.
 
 ## The Firmware
 
-I add a bigger power range icon from 4.2V (full) to 2.7V (empty). There
-is a small tick on it for 3.3V and a bigger tick for 3.7V
-
-I add the temperature mesurement for testing, too.
-
-- Button 2: time, temperatur, power 2.5sec
-- Button 1: sends the request
-- send a %byte from App to get the COUNT of new messages as byte (App > v0.7)
-- is the COUNT > COUNTAPP (config), speaker beeps 1 time and red LED switch on
-- COUNT = 0 switchs LED off
+Is full! there is a game added, but my trinket or the arduino IDE is buggy and
+the code does not work :-(
 
 ## Circuit
 
@@ -22,17 +14,6 @@ The 3.3V regulator (BAT Pin) is be IRGNORED! This means, that the OLED Display
 get 4.2V (a 3.7V LiPo could do that) INSTEAD of 3.3V ! This could break
 the display!
 
-Devices:
-
-- [Adafruit Bluefruit LE UART Friend](https://learn.adafruit.com/introducing-the-adafruit-bluefruit-le-uart-friend)
-- [Micro OLED Breakout](https://github.com/sparkfun/Micro_OLED_Breakout)
-- [Pro Trinket 3V](https://learn.adafruit.com/introducing-pro-trinket)
-- LiPo 3,7V (170mAh)
-
 ![Circuit of the UART-Smartwatch](stuff/circuit.png)
 
 [UART-Smartwatch Circuit PDF](https://raw.githubusercontent.com/no-go/UART-Smartwatch/powerCritical/stuff/UART-Smartwatch.pdf)
-
-# Sign Of Gratitude
-
-Thank you to [NORDIC SEMICONDUCTOR](http://www.nordicsemi.com/) for the OpenSource App like [nRF UART v2](https://github.com/NordicSemiconductor/Android-nRF-UART) to make it easy to use their products!!
--- UART-Smartwatch_firmware/UART-Smartwatch_firmware.ino
+++ UART-Smartwatch_firmware/UART-Smartwatch_firmware.ino
@@ -32,19 +32,12 @@ MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS);
 char memoStr[MEMOSTR_LIMIT] = {'\0'};
 int  memoStrPos   = MESSAGEPOS;
 int  page         = 0;
-byte mode         = 0;
 byte COUNT        = 0;
 
 byte hours = 0;
 byte minutes = 0;
 byte seconds = 0;
 byte tick = 0;
-
-// save power, because sin/cos is to "expensive"
-const byte xHour[13] = {29,34,38,39,38,34,29,24,20,19,20,24,29};
-const byte yHour[13] = {21,22,26,31,36,40,41,40,36,31,26,22,21};
-const byte xMin[60]  = {29,30,32,33,34,35,37,38,39,40,40,41,41,42,42,42,42,42,41,41,40,40,39,38,37,35,34,33,32,30,29,28,26,25,24,22,21,20,19,18,18,17,17,16,16,16,16,16,17,17,18,18,19,20,21,23,24,25,26,28};
-const byte yMin[60]  = {18,18,18,19,19,20,20,21,22,23,24,26,27,28,30,31,32,34,35,36,38,39,40,41,42,42,43,43,44,44,44,44,44,43,43,42,42,41,40,39,37,36,35,34,32,31,30,28,27,26,24,23,22,21,20,20,19,19,18,18};
 
 PROGMEM const char comm[][35] = {
   "+++",
@@ -55,58 +48,14 @@ PROGMEM const char comm[][35] = {
   "ATZ"
 };
 
-void setupBle() {
-  for(byte i=0; i<6; ++i) {
-    Serial.println(comm[i]); 
-    delay(250);  
-  }
-}
-
-void readTemp() {
-  unsigned int wADC;
-  int t;
-  // Set the internal reference and mux.
-  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
-  ADCSRA |= _BV(ADEN);  // enable the ADC
-  delay(20);            // wait for voltages to become stable.
-  ADCSRA |= _BV(ADSC);  // Start the ADC
-  // Detect end-of-conversion
-  while (bit_is_set(ADCSRA,ADSC));
-  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
-  wADC = ADCW;
-  // The offset of 324.31 could be wrong. It is just an indication.
-  t = ( wADC - 324.31 ) / 1.22;
-  // The returned temperature is in degrees Celcius.
-  oled.setCursor(0, 41);
-  oled.print(t);
-  oled.setCursor(11, 37);
-  oled.print((char)248);
-}
-
-/**
- * because there is a power regulator, it is hard to
- * messure the battery power level. If you do not connect bat 
- * to battery and connect batery direkt to 3.3V, you can
- * mesure better, but Display could brake!!
- */
-byte readVcc() {
-  int result;
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(10); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
-  result = ADCL; 
-  result |= ADCH<<8; 
-  result = 1126400L / result;
-  // return (result-2700)/18; // scale: 3310 -> 34, 2710 -> 0 (USB5v or 3.3v BAT Regulator)
-  return (result-2700)/45; // scale: 4205 -> 34, 2710 -> 0
-}
-
-void filler() {
-  for (int i=0; i<MESSAGEPOS; ++i) {
-    memoStr[i] = ' ';
-  }
-}
+void analogClock();
+void readTemp();
+void setupBle();
+void gameOver();
+void game();
+byte readVcc();
+void filler();
+char umlReplace(char inChar);
 
 void setup() {
   pinMode(BUTTON1, INPUT);
@@ -115,8 +64,8 @@ void setup() {
   pinMode(SPKR, OUTPUT);
   
   pinMode(LED_RED, OUTPUT);
-  //pinMode(LED_YELLOW, OUTPUT);
-  //pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 
   digitalWrite(BUTTON1, HIGH);
   digitalWrite(BUTTON2, HIGH);
@@ -131,32 +80,6 @@ void setup() {
   oled.clear(ALL); // Clear the display's internal memory logo
   oled.display();
   filler();
-}
-
-/**
- * the hard way to handle with german(?) UTF-8 stuff
- */
-char umlReplace(char inChar) {
-  if (inChar == -97) {
-    inChar = 224; // ß
-  } else if (inChar == -92) {
-    inChar = 132; // ä
-  } else if (inChar == -74) {
-    inChar = 148; // ö
-  } else if (inChar == -68) {
-    inChar = 129; // ü
-  } else if (inChar == -124) {
-    inChar = 142; // Ä
-  } else if (inChar == -106) {
-    inChar = 153; // Ö
-  } else if (inChar == -100) {
-    inChar = 154; // Ü
-  } else if (inChar == -85) {
-    inChar = 0xAE; // <<
-  } else if (inChar == -69) {
-    inChar = 0xAF; // >>
-  }
-  return inChar;  
 }
 
 void serialEvent() {
@@ -181,32 +104,9 @@ void serialEvent() {
  */
 void ticking() {
   tick++;
-  /*
-  if (mode > 0) {
-    analogWrite(LED_RED, 25*tick);
-  } else {
-    digitalWrite(LED_RED, LOW);
-  }
-  */
 
   if (tick > 9) {
     seconds += tick/10;
-    /*
-    if (mode > 0) {
-      digitalWrite(LED_GREEN, seconds%2 ? LOW:HIGH);
-    } else {
-      digitalWrite(LED_GREEN, LOW);
-    }
-    if (mode == 2) {
-      if (seconds == 60) {
-        analogWrite(SPKR, 140);
-      } else {
-        digitalWrite(SPKR, LOW);      
-      }
-    } else {
-      digitalWrite(SPKR, LOW);
-    }
-    */
 
     tick = tick % 10;
     if (seconds > 59) {
@@ -251,14 +151,6 @@ void digitalClock() {
   oled.print(seconds);  
 }
 
-void analogClock() {
-  oled.circle(29, 31, 13);
-  byte hour = hours;
-  if (hour>12) hour-=12;
-  oled.line(29, 31, xMin[minutes], yMin[minutes]);
-  oled.line(29, 31, xHour[hour], yHour[hour]);  
-}
-
 void batteryIcon() {
   byte vccVal = readVcc();
   oled.pixel(61, 13);
@@ -270,27 +162,102 @@ void batteryIcon() {
   oled.rectFill(60, 48-vccVal, 4, vccVal);  
 }
 
-
-void menu() {
-  mode++;
-  if (mode > 2) mode = 0;
-  
-  oled.clear(PAGE);
-  oled.setCursor(0, 0);
-  oled.println("   MODE  ");
-  oled.print(  "==========");
-  oled.println("- silent ");
-  oled.println("- LEDs   ");
-  oled.println("-  + beep");
-  oled.setCursor(4, 8*mode + 16);
-  oled.print(">");
-  
-  oled.command(DISPLAYON);
-  oled.display();
-  Serial.print("Mode ");
-  Serial.println(mode);
+void printDino(byte x, byte y) {
+  byte i;
+  byte a[] ={
+             2,0, 3,0, 4,0,
+             2,1,      4,1, 5,1,
+             2,2, 3,2, 4,2, 5,2, 6,2,
+             2,3, 3,3, 4,3, 5,3, 6,3,
+             2,4, 3,4, 4,4, 
+             2,5, 3,5, 4,5, 5,5, 6,5,
+   0,6,           3,6, 4,6,
+        1,7, 2,7, 3,7, 4,7,     
+             2,8, 3,8, 4,8, 5,8,
+                  3,9, 4,9
+  };
+  for (i=0; i<sizeof(a); i+=2) {
+    oled.pixel(a[i]+x, a[i+1]+y);
+  }
 }
 
+void subLive() {
+  byte i;
+  byte q[] ={
+                  3,0, 4,0, 5,0,
+        1,1, 2,1, 3,1, 4,1, 5,1,
+        1,2,      3,2,      5,2,
+        1,3, 2,3,      4,3, 5,3,
+        1,4,      3,4,      5,4,
+        1,5, 2,5, 3,5, 4,5, 5,5,
+             2,6, 3,6, 4,6, 5,6,
+                       4,7, 5,7,     
+                  3,8, 4,8, 5,8,
+                  3,9, 4,9, 5,9,
+                     4,10, 5,10,
+         2,11, 3,11, 4,11, 5,11
+  };
+  for (i=0; i<sizeof(q); i+=2) {
+    oled.pixel(26-q[i+1], q[i]+38);
+  }
+}
+
+void feet1(byte x, byte y) {
+  byte i;
+  byte a[] ={ 
+             2,10, 3,10, 4,10, 
+             2,11,           5,11
+  };
+  for (i=0; i<sizeof(a); i+=2) {
+    oled.pixel(a[i]+x, a[i+1]+y);
+  }
+}
+
+void feet2(byte x, byte y) {
+  byte i;
+  byte a[] ={
+             2,10, 3,10, 4,10, 
+                         5,10
+  };
+  for (i=0; i<sizeof(a); i+=2) {
+    oled.pixel(a[i]+x, a[i+1]+y);
+  }
+}
+
+void feet3(byte x, byte y) {
+  byte i;
+  byte a[] ={
+             3,10, 4,10, 
+             3,11,      5,11
+  };
+  for (i=0; i<sizeof(a); i+=2) {
+    oled.pixel(a[i]+x, a[i+1]+y);
+  }
+}
+
+void printCactus(int x, int y) {
+  byte i;
+  int tmp;
+  int a[] = {
+              2,0,
+         1,1, 2,1, 
+              2,2,      4,2,
+    0,3,      2,3,      4,3,
+    0,4,      2,4,      4,4,
+    0,5, 1,5, 2,5, 3,5, 4,5,
+         1,6, 2,6, 3,6, 4,6,
+              2,7, 3,7, 4,7,
+              2,8, 3,8,
+              2,9, 3,9,
+              2,10,
+              2,11
+  };
+  for (i=0; i<sizeof(a); i+=2) {
+    tmp = a[i]+x;
+    if(tmp<0 || tmp>63) continue;
+    oled.pixel(tmp, a[i+1]+y);
+  }
+}
 
 void loop() {
   delay(93); // is < 100 : makes the seconds a bit faster!
@@ -299,8 +266,6 @@ void loop() {
     delay(300);
     tick += 3;
     if (digitalRead(BUTTON2) == LOW) {
-      
-      //menu();
       
       oled.command(DISPLAYON);
       oled.clear(PAGE);
@@ -314,6 +279,13 @@ void loop() {
       delay(2500);
       tick += 25;
       page = memoStrPos;
+      
+      COUNT = 0;
+      digitalWrite(LED_RED, LOW);
+    
+      if (digitalRead(BUTTON2) == LOW) {
+        game();
+      }
     }
   }
 
@@ -331,8 +303,10 @@ void loop() {
       // print ignores everyting behind \0
       memoStr[MESSAGEPOS] = '\0';
       memoStrPos = MESSAGEPOS;
+      
       COUNT = 0;
       digitalWrite(LED_RED, LOW);
+      
       Serial.println( CHAR_TIME_REQUEST );
     }
   }
@@ -365,7 +339,7 @@ void loop() {
       
     } else if (memoStr[MESSAGEPOS] == CHAR_NOTIFY_HINT) {
 
-      // there are 3 new messages !!
+      // there are some new messages !!
       
       COUNT = (unsigned char) memoStr[MESSAGEPOS+1];
       if (COUNT > 0) {
@@ -410,5 +384,217 @@ void loop() {
   
   page++;
   ticking();
+}
+
+// save power, because sin/cos is to "expensive"
+const byte xHour[13] = {29,34,38,39,38,34,29,24,20,19,20,24,29};
+const byte yHour[13] = {21,22,26,31,36,40,41,40,36,31,26,22,21};
+const byte xMin[60]  = {29,30,32,33,34,35,37,38,39,40,40,41,41,42,42,42,42,42,41,41,40,40,39,38,37,35,34,33,32,30,29,28,26,25,24,22,21,20,19,18,18,17,17,16,16,16,16,16,17,17,18,18,19,20,21,23,24,25,26,28};
+const byte yMin[60]  = {18,18,18,19,19,20,20,21,22,23,24,26,27,28,30,31,32,34,35,36,38,39,40,41,42,42,43,43,44,44,44,44,44,43,43,42,42,41,40,39,37,36,35,34,32,31,30,28,27,26,24,23,22,21,20,20,19,19,18,18};
+
+void analogClock() {
+  oled.circle(29, 31, 13);
+  byte hour = hours;
+  if (hour>12) hour-=12;
+  oled.line(29, 31, xMin[minutes], yMin[minutes]);
+  oled.line(29, 31, xHour[hour], yHour[hour]);  
+}
+
+void readTemp() {
+  unsigned int wADC;
+  int t;
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+  delay(20);            // wait for voltages to become stable.
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = ( wADC - 324.31 ) / 1.22;
+  // The returned temperature is in degrees Celcius.
+  oled.setCursor(0, 41);
+  oled.print(t);
+  oled.setCursor(11, 37);
+  oled.print((char)248);
+}
+
+void setupBle() {
+  for(byte i=0; i<6; ++i) {
+    Serial.println(comm[i]); 
+    delay(250);  
+  }
+}
+
+/**
+ * because there is a power regulator, it is hard to
+ * messure the battery power level. If you do not connect bat 
+ * to battery and connect batery direkt to 3.3V, you can
+ * mesure better, but Display could brake!!
+ */
+byte readVcc() {
+  int result;
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(10); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+  result = ADCL; 
+  result |= ADCH<<8; 
+  result = 1126400L / result;
+  // return (result-2700)/18; // scale: 3310 -> 34, 2710 -> 0 (USB5v or 3.3v BAT Regulator)
+  return (result-2700)/45; // scale: 4205 -> 34, 2710 -> 0
+}
+
+void filler() {
+  for (int i=0; i<MESSAGEPOS; ++i) {
+    memoStr[i] = ' ';
+  }
+}
+
+void game() {
+  // sound on
+  power_adc_enable();
+
+  int lives = 3;
+  int score = 0;
+  int jumpY = 0;
+  int cloud = 56;
+
+  short cactus1 = 70;
+
+  oled.clear(PAGE);
+  oled.command(DISPLAYON);
+  
+  oled.setCursor(0, 0);
+  oled.println("   Dino");
+  oled.print(  "==========");  
+  oled.display();
+
+  delay(1000);
+  tick += 10;
+    
+  while(lives > 0) {
+    // score is time :-D
+    score++;
+    
+    oled.clear(PAGE);
+    oled.setCursor(0, 0);
+    oled.print(lives);
+    oled.print("  ");
+    oled.print(score);
+
+    // cloud
+    if (cloud == 0) cloud=56;
+    oled.setCursor(cloud, 10);
+    oled.print("*");
+    if (score%2 == 0) cloud--;
+      
+    // ground
+    oled.line(0, 45, 63, 45);
+
+    // cactus
+    if (cactus1 < -7) cactus1=70;
+    printCactus(cactus1, 33);
+    cactus1-=2;
+
+
+    // collision and die ---------------
+    if (cactus1 == 6 && jumpY<13) {
+      lives--;
+      subLive();
+      oled.display();
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_YELLOW, HIGH);
+      analogWrite(SPKR, 120);
+      delay(500);
+      analogWrite(SPKR, 32);
+      delay(500);
+      tick += 10;
+      digitalWrite(SPKR, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+
+    } else {
+      // alive -------------------------
+      // print body
+      
+      printDino(2, 32-jumpY);
+      // print the trippling feeds
+      if (score%3     == 0) feet1(2, 32-jumpY);
+      if ((score+1)%3 == 0) feet2(2, 32-jumpY);
+      if ((score+2)%3 == 0) feet3(2, 32-jumpY);
+      oled.display();
+    }
+
+    delay(50);
+    if (score%2 == 0) tick += 1;
+
+    // jump animation + sound
+    switch(jumpY) {
+      case 1: jumpY = 7; analogWrite(SPKR, 60); digitalWrite(LED_GREEN, HIGH); break;
+      case 7: jumpY = 11;  break;
+      case 11: jumpY = 13; analogWrite(SPKR, 65); break;
+      case 13: jumpY = 15; analogWrite(SPKR, 85); break;
+      case 15: jumpY = 14;  break;
+      case 14: jumpY = 12; analogWrite(SPKR, 95); break;
+      case 12: jumpY = 10;  break;
+      case 10: jumpY = 9;  digitalWrite(SPKR, LOW); break;
+      case 9:  jumpY = 8; break;
+      case 8:  jumpY = 6; break;
+      case 6: jumpY = 4;  break;
+      case 4: jumpY = 2;  digitalWrite(LED_GREEN, LOW); break;
+      case 2: jumpY = 0; break;
+      default: break;
+    }
+
+    // exit button
+    if (digitalRead(BUTTON1) == LOW) lives = 0;
+
+    // jump button
+    if (jumpY==0 && digitalRead(BUTTON2) == LOW) jumpY = 1;
+
+  }
+  gameOver();
+}
+
+void gameOver() {
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  analogWrite(SPKR, 5);
+  oled.setCursor(0, 22);
+  oled.println(" Game Over");
+  oled.display();
+  delay(3000);
+  tick += 30;  
+  // sound off
+  digitalWrite(SPKR, LOW);
+  power_adc_disable();
+}
+
+/**
+ * the hard way to handle with german(?) UTF-8 stuff
+ */
+char umlReplace(char inChar) {
+  if (inChar == -97) {
+    inChar = 224; // ß
+  } else if (inChar == -92) {
+    inChar = 132; // ä
+  } else if (inChar == -74) {
+    inChar = 148; // ö
+  } else if (inChar == -68) {
+    inChar = 129; // ü
+  } else if (inChar == -124) {
+    inChar = 142; // Ä
+  } else if (inChar == -106) {
+    inChar = 153; // Ö
+  } else if (inChar == -100) {
+    inChar = 154; // Ü
+  } else if (inChar == -85) {
+    inChar = 0xAE; // <<
+  } else if (inChar == -69) {
+    inChar = 0xAF; // >>
+  }
+  return inChar;  
 }
 
