6e3b9f69cf6401d48358630294699a107cdbae07
jochen@homeland <no-go@localhost>
2017-01-10 15:15:52 +0100

    first try with RGB oled display SSD1331

--- README.md
+++ README.md
@@ -1,25 +1,4 @@
 # UART-Smartwatch
 
 This branch is a bit different than the master. Firmware and circuit are
-modified.
-
-## The Firmware
-
-Is full with my private mods.
-
-- Button 1: time and message request
-- Poti: scroll through messages
-- Button 2: shows time and power level for 4 seconds (seconds included)
-- Button 2 (more than 4sec pressed): start Dino Game
-- Dino game: Button 2 for jump, 3 lives, 6 speed levels, stores Highscore in EEPROM
-- Time: Button 2 to switch between digital and analoge Clock!
-
-## Circuit
-
-The 3.3V regulator (BAT Pin) is used! Drive the display with more than 3.3V
-reduce its life time. Drive all parts with 3.3V instead of 3.7V or 4.2V
-ist better -> we need less mAh and LiPo lives longer.
-
-![Circuit of the UART-Smartwatch](stuff/circuit.png)
-
-[UART-Smartwatch Circuit PDF](https://raw.githubusercontent.com/no-go/UART-Smartwatch/powerCritical/stuff/UART-Smartwatch.pdf)
+modified. A RGB oled Display (SSD1331 Chip) is used.
--- UART-Smartwatch_firmware/UART-Smartwatch_firmware.ino
+++ UART-Smartwatch_firmware/UART-Smartwatch_firmware.ino
@@ -1,52 +1,85 @@
-#include <SPI.h>
-#include <SFE_MicroOLED.h>
-#include <avr/power.h>
-#include <EEPROM.h>
 
-// avrdude -c usbtiny -p m328p  -B 300 -U flash:w:UART-Smartwatch_firmware.ino.arduino_eightanaloginputs.hex:i
+#define BUTTON1    3
 
-// ---------------------------- Configure ! -------------
-#define PIN_RESET  9
-#define PIN_DC     8
-#define PIN_CS    10
-#define DC_JUMPER  0
-
-#define BUTTON1     3
-#define BUTTON2     4
-
-#define SPKR        5
-#define LED_RED     6
-#define LED_YELLOW  A1
-#define LED_GREEN   A2
-#define POTI        A4 // scroll throu message instead of page++; 
-
-#define MESSAGEPOS     50 // default:  30 = screen middle
-#define MEMOSTR_LIMIT 550 // default: 730 = 700 char buffer
+#define MESSAGEPOS    120
+#define MEMOSTR_LIMIT 720
 
 #define CHAR_TIME_REQUEST '~'
 #define CHAR_TIME_RESPONSE '#'
-#define CHAR_NOTIFY_HINT '%'
+
+const int batLength = 60;
+
+// ------------------------------------------------------
 
 const int xHour[13] = {32,40,47,49,47,40,32,23,17,15,17,24,32};
 const int yHour[13] = {6,8,14,23,32,38,40,38,31,23,14,8,6};
 const int xMin[60]  = {32,34,36,38,41,42,44,46,48,49,50,51,52,53,53,53,53,53,52,51,50,49,48,46,44,42,41,38,36,34,32,30,28,26,23,21,20,18,16,15,14,13,12,11,11,11,11,11,12,13,14,15,16,18,20,22,23,26,28,30};
 const int yMin[60]  = {2,2,2,3,4,5,6,7,9,11,12,14,17,19,21,23,25,27,29,32,34,35,37,39,40,41,42,43,44,44,44,44,44,43,42,41,40,39,37,35,33,32,29,27,25,23,21,19,17,14,12,11,9,7,6,5,4,3,2,2};
 
-// ------------------------------------------------------
+#define sclk   13
+#define mosi   11
+#define cs     10
+#define rst    9
+#define dc     8
 
-int eeAddress = 0;
-int score     = 0;
-int highscore = 0;
-void game();
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
 
-// display 64x48
-MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS);
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+#include <SPI.h>
+#include <avr/power.h>
+
+
+// Option 1: use any pins but a little slower
+//Adafruit_SSD1331 oled = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);  
+
+// Option 2: must use the hardware SPI pins 
+// (for UNO thats sclk = 13 and sid = 11 ???) and pin 10 must be 
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+// (for Spark Core, cs = A2, dc = D3, rst = D2)
+Adafruit_SSD1331 oled = Adafruit_SSD1331(cs, dc, rst); 
+
+
+/*
+
+// Scroll
+#define ACTIVATESCROLL              0x2F
+#define DEACTIVATESCROLL            0x2E
+#define SETVERTICALSCROLLAREA       0xA3
+#define RIGHTHORIZONTALSCROLL       0x26
+#define LEFT_HORIZONTALSCROLL       0x27
+#define VERTICALRIGHTHORIZONTALSCROLL 0x29
+#define VERTICALLEFTHORIZONTALSCROLL  0x2A
+
+void scrollStop() {
+  oled.writeCommand(DEACTIVATESCROLL);
+}
+
+void scrollUp() {
+  scrollStop();
+  oled.writeCommand(RIGHTHORIZONTALSCROLL);
+  oled.writeCommand(0x00);
+  oled.writeCommand(0x00);  
+  oled.writeCommand(0x40);
+  oled.writeCommand(0x01);
+  oled.writeCommand(0x02);
+  oled.writeCommand(ACTIVATESCROLL);
+}
+
+*/
 
 char memoStr[MEMOSTR_LIMIT] = {'\0'};
 int  memoStrPos   = MESSAGEPOS;
 int  page         = 0;
-
-byte COUNT        = 0;
 
 byte hours = 0;
 byte minutes = 0;
@@ -54,18 +87,8 @@ byte seconds = 0;
 byte tick = 0;
 
 bool usingBATpin;
-bool useAnalogClock = false;
 
-int readWheel() {
-  power_adc_enable();
-  int reading = analogRead(POTI);
-  power_adc_disable();
-  reading = map(reading, 0, 1023, 0, MEMOSTR_LIMIT - MESSAGEPOS);
-  if (reading > memoStrPos) reading = memoStrPos; // make display off
-  return reading;
-}
-
-int readVcc() {
+byte readVcc() {
   int result;
   power_adc_enable();
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
@@ -76,72 +99,68 @@ int readVcc() {
   result |= ADCH<<8; 
   result = 1126400L / result;
   power_adc_disable();
-  return result;
+  return powerTick(result);
+}
+
+byte powerTick(int mv) {
+  float quot = (5100-2700)/batLength; // scale: 5100 -> batLength, 2710 -> 0
+  return (mv-2700)/quot;  
 }
 
 void anaClock() {
-  oled.circle(32, 23, 23);
+  oled.drawCircle(32, 23, 23, WHITE);
   int hour = hours;
   if (hour>12) hour-=12;
-  oled.line(32,   23, xMin[seconds], yMin[seconds]);
+  if (seconds == 0) {
+    oled.drawLine(32, 23, xMin[59], yMin[59], BLACK);
+  } else {
+    oled.drawLine(32, 23, xMin[seconds-1], yMin[seconds-1], BLACK);    
+  }
+  
+  oled.drawLine(32,   23, xMin[seconds], yMin[seconds], RED);
 
-  oled.line(32,   23, xMin[minutes], yMin[minutes]);
-  oled.line(32,   23, xHour[hour],   yHour[hour]);
-  oled.line(32+1, 23, xHour[hour]+1, yHour[hour]);
+  oled.drawLine(32,   23, xMin[minutes], yMin[minutes], CYAN);
+  oled.drawLine(32,   23, xHour[hour],   yHour[hour], YELLOW);
   
   for (int i=0; i<12; ++i) {
-    oled.pixel(xHour[i], yHour[i]);  
+    oled.drawPixel(xHour[i], yHour[i], MAGENTA);  
   }
   // 12 o'clock
-  oled.pixel(30, 3);
-  oled.pixel(30, 4);
-  oled.pixel(30, 5);
-  oled.pixel(30, 6);
-  oled.pixel(32, 3);
-  oled.pixel(33, 3);
-  oled.pixel(33, 4);
-  oled.pixel(32, 5);
-  oled.pixel(33, 6);
-  oled.pixel(34, 6);
+  oled.drawPixel(30, 3, WHITE);
+  oled.drawPixel(30, 4, WHITE);
+  oled.drawPixel(30, 5, WHITE);
+  oled.drawPixel(30, 6, WHITE);
+  oled.drawPixel(32, 3, WHITE);
+  oled.drawPixel(33, 3, WHITE);
+  oled.drawPixel(33, 4, WHITE);
+  oled.drawPixel(32, 5, WHITE);
+  oled.drawPixel(33, 6, WHITE);
+  oled.drawPixel(34, 6, WHITE);
   // 3 o'clock
-  oled.pixel(49, 21);
-  oled.pixel(50, 21);
-  oled.pixel(50, 22);
-  oled.pixel(49, 23);
-  oled.pixel(50, 24);
-  oled.pixel(49, 25);
-  oled.pixel(50, 25);
+  oled.drawPixel(49, 21, WHITE);
+  oled.drawPixel(50, 21, WHITE);
+  oled.drawPixel(50, 22, WHITE);
+  oled.drawPixel(49, 23, WHITE);
+  oled.drawPixel(50, 24, WHITE);
+  oled.drawPixel(49, 25, WHITE);
+  oled.drawPixel(50, 25, WHITE);
   // 6 o'clock
-  oled.pixel(32, 41);
-  oled.pixel(31, 42);
-  oled.pixel(30, 43);
-  oled.pixel(31, 43);
-  oled.pixel(30, 44);
-  oled.pixel(31, 44);
-  oled.pixel(32, 44);
-  oled.pixel(31, 45);
+  oled.drawPixel(32, 41, WHITE);
+  oled.drawPixel(31, 42, WHITE);
+  oled.drawPixel(30, 43, WHITE);
+  oled.drawPixel(31, 43, WHITE);
+  oled.drawPixel(30, 44, WHITE);
+  oled.drawPixel(31, 44, WHITE);
+  oled.drawPixel(32, 44, WHITE);
+  oled.drawPixel(31, 45, WHITE);
   // 9 o'clock
-  oled.pixel(14, 21);
-  oled.pixel(13, 22);
-  oled.pixel(15, 22);
-  oled.pixel(14, 23);
-  oled.pixel(15, 23);
-  oled.pixel(14, 24);
-  oled.pixel(13, 25);
-}
-
-/**
- * because there is a power regulator, it is hard to
- * messure the battery power level. If you do not connect bat 
- * to battery and connect batery direkt to 3.3V, you can
- * mesure better, but Display could brake!!
- */
-byte vcc2hight(int result) {
-  if (usingBATpin) {
-    return (result-2700)/18; // scale: 3310 -> 34, 2710 -> 0 (USB5v or 3.3v BAT Regulator)    
-  } else {
-    return (result-2700)/45; // scale: 4205 -> 34, 2710 -> 0
-  }  
+  oled.drawPixel(14, 21, WHITE);
+  oled.drawPixel(13, 22, WHITE);
+  oled.drawPixel(15, 22, WHITE);
+  oled.drawPixel(14, 23, WHITE);
+  oled.drawPixel(15, 23, WHITE);
+  oled.drawPixel(14, 24, WHITE);
+  oled.drawPixel(13, 25, WHITE);
 }
 
 void filler() {
@@ -150,38 +169,22 @@ void filler() {
   }
 }
 
-void setup() {
+void setup(void) {
   pinMode(BUTTON1, INPUT);
-  pinMode(BUTTON2, INPUT);
-  pinMode(POTI, INPUT);
-
-  pinMode(SPKR, OUTPUT);
   
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-
   digitalWrite(BUTTON1, HIGH);
-  digitalWrite(BUTTON2, HIGH);
   Serial.begin(9600);
 
   power_timer1_disable();
   power_timer2_disable();
   power_adc_disable();
   power_twi_disable();
-
-  int mv = readVcc();
-  usingBATpin = (mv < 3400);
   
   oled.begin();
-  oled.clear(ALL); // Clear the display's internal memory logo
-  oled.display();
+  //scrollUp();
   filler();
 }
 
-/**
- * the hard way to handle with german(?) UTF-8 stuff
- */
 char umlReplace(char inChar) {
   if (inChar == -97) {
     inChar = 224; // ß
@@ -211,7 +214,10 @@ void serialEvent() {
     if (inChar == -61) continue; // symbol before utf-8
     if (inChar == -62) continue; // other symbol before utf-8
     if (inChar == '\n') {
-      oled.command(DISPLAYON);
+      
+      oled.writeCommand(SSD1331_CMD_DISPLAYON);
+      oled.fillScreen(oled.Color565(128, 128, 255));
+      
       memoStr[memoStrPos] = '\0';
       page = 0;
       continue;
@@ -247,40 +253,23 @@ void ticking() {
   }
 }
 
-void digiClock() {
-  oled.setFontType(2);
-  oled.setCursor(0, 12);
-  if (hours<10) oled.print("0");
-  oled.print(hours);
-
-  oled.setFontType(1);
-  oled.setCursor(24, 13);
-  oled.print(":");
-
-  oled.setFontType(2);
-  oled.setCursor(32, 12);
-  if (minutes<10) oled.print("0");
-  oled.print(minutes); 
-
-  oled.setFontType(0);
-  oled.setCursor(17, 32);
-  if (seconds<10) oled.print("0");
-  oled.print(seconds);
-  oled.print(".");
-  oled.print(tick);
-}
-
 void batteryIcon() {
-  byte vccVal = vcc2hight(readVcc());
-  oled.pixel(60, 13);
-  oled.pixel(61, 13);
-  if (usingBATpin == false) {
-    oled.pixel(57, 35); // 3.3V tick
-    oled.pixel(56, 26); // 3.7V tick
-    oled.pixel(57, 26); // 3.7V tick
+  byte vccVal = readVcc();
+  oled.drawPixel(oled.width()-4, oled.height() - batLength -1, WHITE);
+  oled.drawPixel(oled.width()-3, oled.height() - batLength -1, WHITE);
+  oled.fillRect(oled.width()-6, oled.height()  -vccVal,    6,    vccVal, GREEN); 
+  oled.drawRect(oled.width()-6, oled.height()  -batLength, 6, batLength, WHITE);
+
+  int ptick[5] = {50,42,37,33,20};
+  int pos;
+  for(int i=0;i<5;++i) {
+    pos = oled.height() - powerTick(ptick[i]*100);
+    oled.setCursor(oled.width()-30, pos);
+    oled.setTextSize(0);
+    oled.setTextColor(WHITE, BLACK);
+    oled.print(ptick[i]/10.0, 1);
+    oled.drawPixel(oled.width()-7,  pos, WHITE);
   }
-  oled.rect(58, 14, 6, 34);
-  oled.rectFill(58, 48-vccVal, 6, vccVal); 
 }
 
 byte tob(char c) {
@@ -288,60 +277,33 @@ byte tob(char c) {
 }
 
 void loop() {
-  delay(93); // is < 100 : makes the seconds a bit faster!
+  delay(90);
 
-  if (digitalRead(BUTTON2) == LOW) {
+  if (digitalRead(BUTTON1) == LOW) {
     delay(300); 
     tick += 3; 
-    if (digitalRead(BUTTON2) == LOW) {
-      
-      COUNT = 0;
-      
-      oled.command(DISPLAYON);
-
-      for (int j=0; j<40; ++j) { // 4sec
-        oled.clear(PAGE);
-        ticking();
-        if (digitalRead(BUTTON1) == LOW) {
-          useAnalogClock = (useAnalogClock? false : true); // flipflop
-        }
-        if (useAnalogClock) {
-          anaClock();
-        } else {
-          digiClock();
-        }
-        batteryIcon();
-        oled.display();
-        delay(90); // 10ms in vcc mesurement
-      }
-      oled.command(DISPLAYOFF);
-
-      if (digitalRead(BUTTON2) == LOW) {      
-        Serial.println("Game !");
-        oled.command(DISPLAYON);
-        power_adc_enable();
-        game();
-        power_adc_disable();
-        oled.command(DISPLAYOFF);
-      }      
-    }
-  }
-  
-  if (digitalRead(BUTTON1) == LOW) {
-    delay(300);  
-    tick += 3;
     if (digitalRead(BUTTON1) == LOW) {
       
-      COUNT = 0;
-              
-      // "remove" old chars from buffer
-      // print ignores everyting behind \0
-      memoStr[MESSAGEPOS] = '\0';
-      memoStrPos = MESSAGEPOS;
-      Serial.println( CHAR_TIME_REQUEST );
+      oled.writeCommand(SSD1331_CMD_DISPLAYON);
+      oled.fillScreen(BLACK);
+
+      batteryIcon();
+      
+      for (int j=0; j<8; ++j) { // 4sec
+        ticking();
+        anaClock();
+        delay(500);
+      }
+      
+      oled.writeCommand(SSD1331_CMD_DISPLAYOFF);
+      
+      if (digitalRead(BUTTON1) == LOW) {
+        memoStr[MESSAGEPOS] = '\0';
+        memoStrPos = MESSAGEPOS;
+        Serial.println( CHAR_TIME_REQUEST );
+      }
     }
   }
-
   /**
    * React, if there is a new message
    */
@@ -358,309 +320,34 @@ void loop() {
       hours = tob(memoStr[MESSAGEPOS+1])*10 + tob(memoStr[MESSAGEPOS+2]);
       minutes = tob(memoStr[MESSAGEPOS+4])*10 + tob(memoStr[MESSAGEPOS+5]);
       seconds = tob(memoStr[MESSAGEPOS+7])*10 + tob(memoStr[MESSAGEPOS+8]);
-    } else if (memoStr[MESSAGEPOS] == CHAR_NOTIFY_HINT) {
-      
-      // there is a new message (or a message is deleted)
-      
-      COUNT = (unsigned char) memoStr[MESSAGEPOS+1];
-      if (COUNT > 0) {
-      } else {
-        memoStr[MESSAGEPOS] = '\0';
-      }
-      page = memoStrPos; // makes a clear and display off        
-     }
+    }
   }
 
   /**
    * Scrolling message through display
    */
   if (memoStrPos > MESSAGEPOS && page <= memoStrPos) {
-    oled.clear(PAGE);
     oled.setCursor(0, 0);
+    oled.setTextSize(0);
+    oled.setTextColor(oled.Color565(192, 192, 255), oled.Color565(0, 0, 48));
     oled.print(&(memoStr[page]));
-    oled.display();
+    oled.print(" ");
   }
 
   /// Safe power and switch display off, if message is at the end
   if (page == memoStrPos) {
-    oled.command(DISPLAYOFF);
+    
+    oled.writeCommand(SSD1331_CMD_DISPLAYOFF);
+    
     // "remove" old chars from buffer
     // print ignores everyting behind \0
     memoStr[MESSAGEPOS] = '\0';
     memoStrPos = MESSAGEPOS;
   }
-
-  if (COUNT > 0) {
-    if (tick%9 == 0) {
-      digitalWrite(LED_YELLOW, HIGH);
-    } else {
-      digitalWrite(LED_YELLOW, LOW);      
-    }  
-  }
    
-  page = readWheel();
+  page += 15;
+  if (page > memoStrPos) page = memoStrPos;
+  
   ticking();
-}
-
-// =====================================================================
-
-void setByte(byte & b, int x, int y) {
-  int tmp;
-  if (x<0 || x>63 || y<0 || y>47) return;
-  for (byte bitNr=0; bitNr<8; ++bitNr) {
-    if (((b >> bitNr) & 0x01)) {
-      tmp = 7-bitNr+x;
-      if (tmp<0 || tmp>63) continue;
-      oled.pixel(tmp, y);
-    }
-  }
-}
-
-void setByte90(byte & b, int x, int y) {
-  int tmp;
-  if (x<0 || x>63 || y<0 || y>47) return;
-  for (byte bitNr=0; bitNr<8; ++bitNr) {
-    if (((b >> bitNr) & 0x01)) {
-      tmp = 7-bitNr+y;
-      if (tmp<0 || tmp>47) continue;
-      oled.pixel(x, tmp);
-    }
-  }
-}
-
-void gameStart() {
-  oled.clear(PAGE);
-  EEPROM.get(eeAddress, highscore);
-
-  oled.setCursor(0, 0);
-  oled.println("   Dino");
-  oled.print(  "==========");
-  oled.println("Highscore:");
-  oled.print(highscore);
-  oled.display();
-
-  delay(3000); // to see the highscore!
-  tick += 30;
-}
-
-void gameOver() {
-  if (score > highscore) {
-    // store it in a persistent flash ROM
-    highscore = score;
-    EEPROM.put(eeAddress, highscore);
-  }
-  digitalWrite(LED_GREEN, LOW);
-  analogWrite(SPKR, 5);
-  oled.setCursor(0, 0);
-  oled.print("0");
-  oled.setCursor(0, 22);
-  oled.println(" Game Over");
-  oled.display();
-  delay(1500);
-  digitalWrite(SPKR, LOW);
-  delay(2000); // read your score
-  tick += 35;
-}
-
-void dino(byte y) {
-  byte i;
-  byte a[] ={
-    0b00111000,
-    0b00101100,
-    0b00111110,
-    0b00111110,
-    0b00111000,
-    0b00111110,
-    0b10011000,
-    0b01111000,
-    0b00111100,
-    0b00011000
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte(a[i], 2, i+y);
-  }
-}
-
-void died() {
-  byte i;
-  byte a[] ={
-    0b00011100,
-    0b01111100,
-    0b01010100,
-    0b01101100,
-    0b01010100,
-    0b01111100,
-    0b00111100,
-    0b00001100,
-    0b00011100,
-    0b00011100,
-    0b00001100,
-    0b00111100
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte90(a[i], 26-i, 38);
-  }
-}
-
-void feet1(byte y) {
-  byte i;
-  byte a[] ={ 
-    0b00111000,
-    0b00100100
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte(a[i], 2, i+y);
-  }
-}
-
-void feet2(byte y) {
-  byte i;
-  byte a[] ={
-    0b01111000,
-    0b00001000
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte(a[i], 2, i+y);
-  }
-}
-
-void feet3(byte y) {
-  byte i;
-  byte a[] ={
-    0b00110000,
-    0b00101000
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte(a[i], 2, i+y);
-  }
-}
-
-void printCactus(int & x) {
-  byte i;
-  int tmp;
-  byte a[] = {
-    0b00100000,
-    0b01100000,
-    0b00101000,
-    0b10101000,
-    0b10101000,
-    0b11111000,
-    0b01111000,
-    0b00111000,
-    0b00110000,
-    0b00110000,
-    0b00100000,
-    0b00100000
-  };
-  for (i=0; i<sizeof(a); ++i) {
-    setByte(a[i], x, i+33);
-  }
-}
-
-void game() {
-  score = 0;
-  int gamespeed;
-  int lives = 3;
-  int jumpY = 0;
-  int cloud = 56;
-  int cactus1 = 70;
-  int subTick = 0;
-
-  gameStart();
-    
-  while(lives > 0) {
-    // score is time :-D
-    score++;
-    
-    oled.clear(PAGE);
-    oled.setCursor(0, 0);
-    oled.print(lives);
-    oled.print("  ");
-    oled.print(score);
-
-    // cloud
-    if (cloud == 0) cloud=56;
-    oled.setCursor(cloud, 10);
-    oled.print("*");
-    if (score%2 == 0) cloud--;
-      
-    // ground
-    oled.line(0, 45, 63, 45);
-
-    // cactus
-    if (cactus1 < -7) cactus1=70;
-    printCactus(cactus1);
-    cactus1-=2;
-
-
-    // collision and die ---------------
-    if (cactus1 == 6 && jumpY<13) {
-      lives--;
-      died();
-      
-      oled.display();
-      digitalWrite(LED_GREEN, LOW);
-      analogWrite(SPKR, 120);
-      delay(500);
-      analogWrite(SPKR, 32);
-      delay(500);
-      digitalWrite(SPKR, LOW);
-      tick += 10;
-      
-    } else {
-      // a good jump?
-      if (jumpY>=13 && cactus1 == 6) score += jumpY;
-      
-      // alive -------------------------      
-      dino(32-jumpY);
-      // print the trippling feeds
-      if (score%3     == 0) feet1(42-jumpY);
-      if ((score+1)%3 == 0) feet2(42-jumpY);
-      if ((score+2)%3 == 0) feet3(42-jumpY);
-      oled.display();
-    }
-
-    // speedup ?!
-    if (score > 4000) {
-      gamespeed = 10;
-    } else if (score > 3000) {
-      gamespeed = 20;
-    } else if (score > 1500) {
-      gamespeed = 30;
-    } else if (score > 1000) {
-      gamespeed = 40;
-    } else if (score > 500) {
-      gamespeed = 50;
-    } else {
-      gamespeed = 70;
-    }
-    
-    subTick += gamespeed;
-    delay(gamespeed);
-    if (subTick > 100) {
-      subTick = 0; ticking();
-    }
-
-    // jump animation + sound
-    switch(jumpY) {          
-      case 1: jumpY = 7; analogWrite(SPKR, 60); digitalWrite(LED_GREEN, HIGH); break;
-      case 7: jumpY = 11;  break;
-      case 11: jumpY = 13; analogWrite(SPKR, 65); break;
-      case 13: jumpY = 15; analogWrite(SPKR, 85); break;
-      case 15: jumpY = 14;  break;
-      case 14: jumpY = 12; analogWrite(SPKR, 95); break;
-      case 12: jumpY = 10;  break;
-      case 10: jumpY = 9;  digitalWrite(SPKR, LOW); break;
-      case 9:  jumpY = 8; break;
-      case 8:  jumpY = 6; break;
-      case 6: jumpY = 4;  break;
-      case 4: jumpY = 2;  digitalWrite(LED_GREEN, LOW); break;
-      case 2: jumpY = 0; break;
-    }
-
-    // jump button
-    if (jumpY==0 && digitalRead(BUTTON2) == LOW) jumpY = 1;
-  }
-  gameOver();
 }
 
